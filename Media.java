dac40446f9cf4e4070c10ee047ab562dd11206e3
Marcus Paulo <104401142+oMarcusPaulo@users.noreply.github.com>
2023-05-07 20:50:44 -0400

    primeiro commit

---
+++ Media.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Media {
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        double a,b,soma,media;
+
+        System.out.println("Digite um valor pra A");
+        a = input.nextDouble();
+        System.out.println("Digite um valor para B: ");
+        b = input.nextDouble();
+
+        soma = a+b;
+        media = soma/2;
+
+        System.out.println("A média é: "+media);
+
+    }
+}
