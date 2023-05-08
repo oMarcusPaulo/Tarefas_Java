f9ed3437940fba7f8131fcb8f231f550cdcb6cb0
Marcus Paulo <104401142+oMarcusPaulo@users.noreply.github.com>
2023-05-07 21:13:23 -0400

    Primeiro ex da lista

---
+++ Pares.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Pares{
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        int a[] = new int [10],b[] = new int[10],i;
+
+        for(i = 0; i<10;i++){
+            System.out.println("Digite valores por favor: ");
+            a[i] = input.nextInt();
+        }
+        for(i = 0; i<10;i++){
+            if(a[i] %2 == 0){
+                b[i] = a[i];
+                System.out.println("O número "+b[i]+" é par");
+            }
+        }
+    }
+}
