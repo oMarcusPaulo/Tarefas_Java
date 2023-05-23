8895c0654a6d73348db7c828d791c5006544fb24
Marcus Paulo <104401142+oMarcusPaulo@users.noreply.github.com>
2023-05-23 02:25:46 -1100

    Create PulaQuinhentos.java

---
+++ PulaQuinhentos.java
@@ -0,0 +1,10 @@
+public class PulaQuinhentos {
+    public static void main(String[]args){
+        for(int i = 1; i<=499;i++){
+            System.out.println(i);
+        }
+        for(int n = 600; n<=1000;n++){
+            System.out.println(n);
+        }
+    }
+}
