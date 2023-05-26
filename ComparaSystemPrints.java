b63bd2ae92ce4fab1d66b53ffeaa8c364af0e060
Marcus Paulo <104401142+oMarcusPaulo@users.noreply.github.com>
2023-05-25 21:43:18 -0400

    se pá tá errada, irei corrigir depois

---
+++ ComparaSystemPrints.java
@@ -0,0 +1,10 @@
+//LISTA 2
+//8 - Crie um programa em Java que utilize as funções
+//System.out.println e System.out.print. Faça a comparação entre estas duas funções.
+
+public class ComparaSystemPrints {
+    public static void main(String[]args){
+        System.out.println("O System.out.println quebra a linha");
+        System.out.print("Essa já não quebra");
+    }
+}
