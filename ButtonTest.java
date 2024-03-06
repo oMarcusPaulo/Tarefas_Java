12c90928ef206ba8709cd91a3ab9f80200cd6d28
Marcus Paulo <104401142+oMarcusPaulo@users.noreply.github.com>
2024-03-06 15:06:40 -0400

    Criando o ButtonTest.java

---
+++ ButtonTest.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class ButtonTest {
+  public static void main(String [] args){
+    ButtonFrame buttonFrame = new ButtonFrame();
+    buttonFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    buttonFrame.setSize(5000, 1000);
+    buttonFrame.setVisible(true);
+  }  
+}
